0d9de33d5b117dc108d15bd9c95815dbb2a48421
s31552 <s31552@pjwstk.edu.pl>
2025-03-18 13:57:21 +0100

    Add fields, constructor; Implement compute and learn methods

---
+++ src/Perceptron.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Perceptron {
+    private List<Double> weights;
+    private double threshold; // theta
+    private double alpha;
+    private double beta;
+    private int dimension;
+
+    public Perceptron(int dimension) {
+        this.dimension = dimension;
+        this.alpha = 0.01;
+        this.beta = 0.01;
+        this.threshold = Math.random();
+        weights = new ArrayList<>();
+
+        for (int i = 0; i < dimension; i++)
+            weights.add(Math.random());
+
+    }
+
+    public int compute(List<Double> inputs) {
+        if (inputs.size() != dimension) {
+            System.out.println("Inputs size != dimension");
+            return -1;
+        }
+
+        double sum = 0;
+        for (int i = 0; i < dimension; i++)
+            sum += inputs.get(i) * weights.get(i);
+
+        double net = sum - threshold;
+
+        if (net >= 0)
+            return 1;
+        else
+            return 0;
+    }
+
+    public void learn(List<Double> inputs, int decision) {
+        if (inputs.size() != dimension) {
+            System.out.println("Inputs size != dimension");
+            return;
+        }
+
+        List<Double> new_weights = weights;
+        int y = compute(inputs);
+
+        for (int i = 0; i < dimension; i++) {
+            new_weights.set(i, new_weights.get(i) + alpha * (decision - y) * inputs.get(i));
+        }
+
+        weights = new_weights;
+        threshold -= alpha * (decision - y);
+    }
+
+
+}
