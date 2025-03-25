5ca2837a4fbb43158aa5ea37f5fcec149bd4f61b
s31552 <s31552@pjwstk.edu.pl>
2025-03-25 10:02:57 +0100

    Add Evaluator class

---
+++ src/Evaluator.java
@@ -0,0 +1,11 @@
+public class Evaluator {
+    private final String testFileName;
+
+    public Evaluator(String testFileName) {
+        this.testFileName = testFileName;
+    }
+
+    public void evaluate(Perceptron perceptron) {
+
+    }
+}
